d6473a8e0fa9720fa5f25a1f565b428f1dce3e5a
lvpetrosyan <liana.liil@mail.ru>
2022-09-19 22:09:57 +0300

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,25 @@ public class Main {
         System.out.println(cat);
         paper = paper - 7639;
         System.out.println(paper);
+        var oneBoxer = 78.2;
+        var twoBoxer = 82.7;
+        var total = oneBoxer + twoBoxer;
+        System.out.println("общий вес 2 бойцов " + total + "кг.");
+        var difference = oneBoxer - twoBoxer;
+        System.out.println("разница между весами 2 бойцов "+ difference + " кг!");
+        var way1 = twoBoxer - oneBoxer;
+        System.out.println("разница между весами 2 спортсменов, 1 вариант расчета "+ way1);
+        var way2= twoBoxer / oneBoxer;
+        System.out.println("Второй вариант расчета. Разница между весами спортсменов " + way2 + " %");
+        var time = 640;
+        var hour = 8;
+        var worker = time / hour;
+        System.out.println("Всего работников в компании - " + worker + " человек.");
+        var dopWorker = 94;
+        var allWorker = worker + dopWorker;
+        var allTime = hour * allWorker;
+        System.out.println("Если в компании работет " + allWorker + " человек, то всего " + allTime + " часов работы может быть поделено между сотрудниками");
+
 
 
 
